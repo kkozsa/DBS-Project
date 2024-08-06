497859755c52f3a0ec390871ec5b059b20e4f5f7
kriszk <kkozsa@gmail.com>
2024-08-06 14:16:13 +0100

    stocks added on portfolio

--- app.py
+++ app.py
@@ -1,17 +1,24 @@
 import yfinance as yf
 import mysql.connector
+from datetime import datetime
 from flask import Flask, render_template, request, redirect, url_for, jsonify, session
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.secret_key = b'11223344'
 
-                                                        # Database config
+
+# Database config
+
 mysql_host = 'localhost'
 mysql_user = 'sqluser'
+#mysql_user = 'web'                           
 mysql_password = '123456789'
 mysql_db = 'sandc_db'
+#mysql_password=input('Enter mySQL password: ')
 
-                                                        # Connect to MySQL
+
+# Connect to MySQL
+
 mysql_conn = mysql.connector.connect(
     host=mysql_host,
     user=mysql_user,
@@ -19,107 +26,194 @@ mysql_conn = mysql.connector.connect(
     database=mysql_db
 )
 
-                                                        # Routes
+
+# Main route
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
+# Login route
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == 'POST':
+    if request.method == 'POST':                    # User submits login form
         email = request.form['email']
         password = request.form['password']
 
-        cursor = mysql_conn.cursor()
-        cursor.execute("SELECT * FROM users WHERE email = %s", (email,))
+        cursor = mysql_conn.cursor()                # Cursor interacts with DB
+        cursor.execute("SELECT * FROM users WHERE email = %s", (email,))        # Query
         user = cursor.fetchone()
         cursor.close()
 
-        if user and user[3] == password:  # Assuming password is the 4th field
-            session['email'] = email  # Store user's email in session
+        if user and user[3] == password:  
+            session['email'] = email  
             return redirect(url_for('portfolio'))
         else:
             return "Invalid credentials"
 
     return render_template('login.html')
 
-@app.route('/portfolio')
+
+# Portfolio route
+
+@app.route('/portfolio', methods=['GET', 'POST'])                   
 def portfolio():
-    return render_template('portfolio.html')
+    if 'email' not in session:                      # Check if the user is logged in
+        return redirect(url_for('login'))
+
+    if request.method == 'POST':
+                                                            # Get userid from the da based session email
+        email = session['email']                            # Identify user by email
+        cursor = mysql_conn.cursor()
+        cursor.execute("SELECT userid FROM users WHERE email = %s", (email,))
+        userid = cursor.fetchone()[0]                       # userid first column
+        cursor.close()
+        content = request.json
+        ticker = content.get('ticker')                                              # Get ticker from the form
+
+                                                                                        # Insert ticker into user_portfolio table
+        cursor = mysql_conn.cursor()
+        cursor.execute("INSERT INTO user_portfolio (userid, ticker) VALUES (%s, %s)", (userid, ticker))
+        mysql_conn.commit()
+        cursor.close()
+        whatever={"result":"success"}
+        return jsonify(whatever)
+
+    email = session['email']
+    cursor = mysql_conn.cursor()
+    cursor.execute("SELECT userid FROM users WHERE email = %s", (email,))
+    userid = cursor.fetchone()[0]  
+
+    cursor = mysql_conn.cursor()
+    cursor.execute("SELECT ticker FROM user_portfolio WHERE userid = %s", (userid,))
+    user_tickers = cursor.fetchall()
+    cursor.close()
+    print (user_tickers)
+    return render_template('portfolio.html', userid=userid, user_tickers=user_tickers)
+
+
+# Tickers route (tickers to database)
+
+@app.route('/tickers', methods=['GET'])                                                 
+def tickers():
+    email = session['email']
+    cursor = mysql_conn.cursor()
+    cursor.execute("SELECT userid FROM users WHERE email = %s", (email,))
+    userid = cursor.fetchone()[0]  
+
+    cursor = mysql_conn.cursor()
+    cursor.execute("SELECT ticker FROM user_portfolio WHERE userid = %s", (userid,))
+    user_tickers = cursor.fetchall()
+    cursor.close()
+    whatever = {'tickers': list(map (lambda x:x[0],user_tickers))}
+    return jsonify(whatever)
+
+
+# Remove tickers route (Remove tickers from database)
+
+@app.route('/remove_ticker', methods=['POST'])
+def remove_ticker():
+    if 'email' not in session:
+        return jsonify({'error': 'Unauthorized'}), 403
+    
+    email = session['email']
+    content = request.json
+    ticker = content.get('ticker')
+
+    cursor = mysql_conn.cursor()
+    cursor.execute("SELECT userid FROM users WHERE email = %s", (email,))
+    userid = cursor.fetchone()[0]
+
+    cursor.execute("DELETE FROM user_portfolio WHERE userid = %s AND ticker = %s", (userid, ticker))
+    mysql_conn.commit()
+    cursor.close()
+
+    return jsonify({'result': 'success'})
+
+
+# Transaction route (delete or improve later)
 
 @app.route('/transactions')
 def transactions():
     return render_template('transactions.html')
 
+
+# Logout route
+
 @app.route('/logout')
 def logout():
-    session.clear()                         # Clear the session to remove user data
+    session.clear()                         # Clear user data
     return redirect(url_for('login'))
+
+
+# Register route (Register details to database)
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    if request.method == 'POST':
+    if request.method == 'POST':                    # User submits register form
         username = request.form['username']
         email = request.form['email']
         password = request.form['password']
         confirm_password = request.form['confirm_password']
 
-        if password != confirm_password:
+        if password != confirm_password:            # Checking password match
             return "Passwords do not match!"
 
-        cursor = mysql_conn.cursor()
-        cursor.execute("SELECT * FROM users WHERE email = %s", (email,))
-        existing_user = cursor.fetchone()
+        cursor = mysql_conn.cursor()                        # Cursor interacts with DB
+        cursor.execute("SELECT * FROM users WHERE email = %s", (email,))        # Query
+        existing_user = cursor.fetchone()                               # Check if user already exists by email
 
         if existing_user:
             cursor.close()
             return "User with this email already exists!"
 
-        cursor.execute("INSERT INTO users (username, email, password) VALUES (%s, %s, %s)", (username, email, password))
+        cursor.execute("INSERT INTO users (username, email, password) VALUES (%s, %s, %s)", (username, email, password))    # Query inserts into user table
         mysql_conn.commit()
         cursor.close()
 
-        return redirect(url_for('login'))
+        return redirect(url_for('login'))               # Back to login page   Maybe change to INDEX
 
-    return render_template('register.html')
+    return render_template('register.html')             # Registration for rendered if GET method
 
-from datetime import datetime
+
+# Profile route (Profile details to database)
 
 @app.route('/profile', methods=['GET', 'POST'])
 def profile():
-    if 'email' not in session:
+    if 'email' not in session:                          # Check if the user already logged in
         return redirect(url_for('login'))
 
-    email = session['email']
+    email = session['email']                            
     
-    if request.method == 'POST':
+    if request.method == 'POST':                        # User submits profile form
         full_name = request.form['full_name']
         date_of_birth = request.form['date_of_birth']
         phone_number = request.form['phone_number']
         address = request.form['address']
 
-        cursor = mysql_conn.cursor()
+        cursor = mysql_conn.cursor()                    # Cursor interacts with DB
         cursor.execute("""
-            UPDATE users SET full_name = %s, date_of_birth = %s, phone_number = %s, address = %s
+            UPDATE users SET full_name = %s, date_of_birth = %s, phone_number = %s, address = %s        
             WHERE email = %s
-        """, (full_name, date_of_birth, phone_number, address, email))
+        """, (full_name, date_of_birth, phone_number, address, email))      # Profile updated in DB if email matches.
         mysql_conn.commit()
         cursor.close()
 
         return redirect(url_for('profile'))
     
-    cursor = mysql_conn.cursor()
-    cursor.execute("SELECT full_name, date_of_birth, phone_number, address FROM users WHERE email = %s", (email,))
+    cursor = mysql_conn.cursor()                    # Get to fetch profile information
+    cursor.execute("SELECT full_name, date_of_birth, phone_number, address FROM users WHERE email = %s", (email,))      # Profile fetch from DB if email matches.
     user_details = cursor.fetchone()
     cursor.close()
 
-    # Format date_of_birth to YYYY-MM-DD if it exists
-    if user_details[1]:
-        date_of_birth = user_details[1].strftime('%Y-%m-%d')
+    if user_details[1]:                                             # Format date_of_birth to YYYY-MM-DD
+        date_of_birth = user_details[1].strftime('%Y-%m-%d')        # 
     else:
-        date_of_birth = ""
+        date_of_birth = ""                                          # If not exists, set to empty string
 
-    user = {
+    user = {                                                        # Dictionary for user profile information
         'full_name': user_details[0],
         'date_of_birth': date_of_birth,
         'phone_number': user_details[2],
@@ -129,12 +223,12 @@ def profile():
     return render_template('profile.html', user=user)
 
 
-
+# Get stock data route (Fetch historical stock price using yahoo finance)
 
 @app.route('/get_stock_data', methods=['POST'])
 def get_stock_data():
-    ticker = request.json['ticker']
-    data = yf.Ticker(ticker).history(period='1y')
+    ticker = request.json['ticker']                     # Extract the value ticker from json data
+    data = yf.Ticker(ticker).history(period='1y')               # Check "1y" options later
     return jsonify({'currentPrice': data.iloc[-1].Close,
                     'openPrice': data.iloc[-1].Open})
 
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,59 +1,150 @@
+/*!
+* Start Bootstrap - Simple Sidebar v6.0.6 (https://startbootstrap.com/template/simple-sidebar)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-simple-sidebar/blob/master/LICENSE)
+*/
 
-window.addEventListener('DOMContentLoaded', event => {
 
-    // Toggle the side navigation
+// Toggle the side navigation
+
+window.addEventListener('DOMContentLoaded', event => {    
     const sidebarToggle = document.body.querySelector('#sidebarToggle');
-    if (sidebarToggle) {
-        // Uncomment Below to persist sidebar toggle between refreshes
-        // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
-        //     document.body.classList.toggle('sb-sidenav-toggled');
-        // }
+    if (sidebarToggle) {        
         sidebarToggle.addEventListener('click', event => {
             event.preventDefault();
             document.body.classList.toggle('sb-sidenav-toggled');
             localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
         });
     }
-
 });
 
+//var tickers = ['AAPL', 'GOOGL', 'MSFT', 'AMZN', 'TSLA', 'NVDA', 'INTC', 'PLTR', 'WCBR', 'SPY', 'ETH-USD', 'BTC-USD'];  // Array of stocks
 
 
+// Update prices every minute
 
-
-var tickers = ['AAPL', 'GOOGL', 'MSFT', 'AMZN', 'TSLA', 'NVDA', 'INTC', 'PLTR', 'WCBR', 'SPY', 'ETH-USD', 'BTC-USD']; // List of popular stocks
-
-function startUpdateCycle() {
+function startUpdateCycle() {               
     updatePrices();
     setInterval(function () {
-        updatePrices(); // Update prices every minute
+        updatePrices();                   
     }, 60000)
 }
 
-$(document).ready(function () {
+
+// Index page, 
+
+$(document).ready(function () {             
     tickers.forEach(function (ticker) {
-        addTickerToGrid(ticker);
+        addTickerToGrid(ticker);        
     });
-
-    updatePrices();
-
-    startUpdateCycle();
+    updatePrices();                         // Call updateprices
+    startUpdateCycle();                     // Call price update 60s
 
     $('#add-ticker-form').submit(function (e) {
         e.preventDefault();
         var newTicker = $('#new-ticker').val().toUpperCase();
         if (!tickers.includes(newTicker)) {
             tickers.push(newTicker);
-            addTickerToGrid(newTicker);
+            localStorage.setItem('tickers', JSON.stringify(tickers))
+            addTickerToGrid(newTicker);            
         }
         $('#new-ticker').val('');
         updatePrices();
     });
 });
 
+
+// Portfolio page
+
+$(document).ready(function () {             // When document ready      
+    fetch ('/tickers').then(data=>data.json())      
+    .then(data=>data['tickers'])
+    .then (tickers => tickers.forEach(function (ticker) {
+        addLiveTicker1(ticker);        
+    }));
+    updatePrices();                         // Call updateprices
+    startUpdateCycle();                     // Call price update 60s
+
+    $('#add-ticker-form2').submit(
+        function (e) {
+        e.preventDefault();
+        var newTicker2 = $('#new-ticker2').val().toUpperCase();
+        addLiveTicker(newTicker2);
+
+        
+        $('#new-ticker2').val('');
+        updatePrices();
+
+    });
+
+    let addLiveTicker=(newTicker2) =>{                  // Adding a new ticker, POST request to server to update portfolio
+    if (!tickers.includes(newTicker2)) {
+        tickers.push(newTicker2);
+        fetch('/portfolio', {
+            method: 'POST',
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ticker: newTicker2})
+          })
+            .then (data => data.json())
+            .then (data => {})
+            .catch (_=> alert("Test alert"));                               
+        localStorage.setItem('tickers', JSON.stringify(tickers))
+        addTickerToGrid2(newTicker2);            
+        }
+    }
+
+    let addLiveTicker1=(newTicker2) =>{
+    if (!tickers.includes(newTicker2)) {
+        tickers.push(newTicker2);
+        localStorage.setItem('tickers', JSON.stringify(tickers))
+        addTickerToGrid2(newTicker2);
+        updatePrices();
+    }}
+
+    $('#tickers-grid2').on('click', '.remove-btn', function () {         // click event listener for elements with class remove-btn
+        var tickerToRemove = $(this).data('ticker');                    // When a user clicks the "Remove" button associated with a ticker, this function executes.
+        tickers=tickers.filter(t => t !== tickerToRemove);              
+        
+        fetch('/remove_ticker', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ ticker: tickerToRemove })
+        })
+            .then(data => data.json())
+            .then(data => {
+                if (data.result === 'success') {
+                    localStorage.setItem('tickers', JSON.stringify(tickers));
+                    $(`#${tickerToRemove}`).remove();
+                } else {
+                    alert('Failed to remove ticker');
+                }
+            })
+            .catch(_ => alert('Failed to remove ticker'));
+    });
+});
+
+
+// Index page grid
+
 function addTickerToGrid(ticker) {
     $('#tickers-grid').append(`<div id="${ticker}" class="stock-box"><h2>${ticker}</h2><p id="${ticker}-price"></p><p id="${ticker}-pct"></p></div>`)
+}                                                           // Function adds ticker box to html grid. ID, name, placeholder for price and % change
+
+
+// Portfolio page grid
+
+function addTickerToGrid2(ticker) {
+    $('#tickers-grid2').append(`<div id="${ticker}" class="stock-box"><h2>${ticker}</h2><p id="${ticker}-price"></p><p id="${ticker}-pct"></p><button class="remove-btn" data-ticker="${ticker}">Remove</button></div>`)
 }
+
+
+// Send AJAX request to fetch stock data for tickers. Update price change
 
 function updatePrices() {
     tickers.forEach(function (ticker) {
